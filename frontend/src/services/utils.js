7377279f9e8ca8e1401fe56e1c1a9cdcd1b68342
Costin Bleotu <costi.bleotu@gmail.com>
2021-10-13 16:45:00 +0300

    Added CI

--- frontend/src/services/utils.js
+++ frontend/src/services/utils.js
@@ -12,87 +12,87 @@ const Colors = {
 }
 
 const DummyFilters = {
-    "basic": {
-        "judete": [
-            {
-                "id": 3,
-                "value": "Arad"
-            }
-        ],
-        "localitati": [
-            {
-                "id": 8,
-                "value": "Corbesti",
-                "judet": 3
-            }
-        ],
-        "conservare": [],
-        "valoare": [],
-        "prioritizare": []
+  basic: {
+    judete: [
+      {
+        id: 3,
+        value: 'Arad',
+      },
+    ],
+    localitati: [
+      {
+        id: 8,
+        value: 'Corbesti',
+        judet: 3,
+      },
+    ],
+    conservare: [],
+    valoare: [],
+    prioritizare: [],
+  },
+  advanced: [
+    {
+      title: 'Identificare',
+      key: 'identificare',
+      filters: [],
     },
-    "advanced": [
+    {
+      title: 'Istoric',
+      key: 'istoric',
+      filters: [],
+    },
+    {
+      title: 'Descriere Arhitectură / Peisaj',
+      key: 'descriere',
+      filters: [
         {
-            "title": "Identificare",
-            "key": "identificare",
-            "filters": []
+          title: 'Amplasament',
+          key: 'amplasament',
+          values: [
+            {
+              id: 1,
+              nume: 'În cadrul așezării',
+            },
+          ],
         },
         {
-            "title": "Istoric",
-            "key": "istoric",
-            "filters": []
+          title: 'Topografie',
+          key: 'topografie',
+          values: [
+            {
+              id: 2,
+              nume: 'La înălțime',
+            },
+          ],
         },
         {
-            "title": "Descriere Arhitectură / Peisaj",
-            "key": "descriere",
-            "filters": [
-                {
-                    "title": "Amplasament",
-                    "key": "amplasament",
-                    "values": [
-                        {
-                            "id": 1,
-                            "nume": "În cadrul așezării"
-                        }
-                    ]
-                },
-                {
-                    "title": "Topografie",
-                    "key": "topografie",
-                    "values": [
-                        {
-                            "id": 2,
-                            "nume": "La înălțime"
-                        }
-                    ]
-                },
-                {
-                    "title": "Relatia Cu Cimitirul",
-                    "key": "relatia_cu_cimitirul",
-                    "values": [
-                        {
-                            "id": 1,
-                            "nume": "În cadrul cimitirului"
-                        }
-                    ]
-                }
-            ]
+          title: 'Relatia Cu Cimitirul',
+          key: 'relatia_cu_cimitirul',
+          values: [
+            {
+              id: 1,
+              nume: 'În cadrul cimitirului',
+            },
+          ],
         },
-        {
-            "title": "Descriere Componenta Artistică",
-            "key": "componenta_artistica",
-            "filters": []
-        },
-        {
-            "title": "Conservare",
-            "key": "conservare",
-            "filters": []
-        },
-        {
-            "title": "Valoare",
-            "key": "valoare",
-            "filters": []
-        }
-    ]
+      ],
+    },
+    {
+      title: 'Descriere Componenta Artistică',
+      key: 'componenta_artistica',
+      filters: [],
+    },
+    {
+      title: 'Conservare',
+      key: 'conservare',
+      filters: [],
+    },
+    {
+      title: 'Valoare',
+      key: 'valoare',
+      filters: [],
+    },
+  ],
 }
 
 export { Colors, DummyFilters }
